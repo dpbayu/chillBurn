46a159a8fe50a76747842387b74ee670fc6743f0
Dwi Putra Bayu <dwiputrabayu19@gmail.com>
2023-07-21 10:27:55 +0700

    change icon & logo

--- admin/partials/head.php
+++ admin/partials/head.php
@@ -8,6 +8,7 @@
         $admin = mysqli_fetch_assoc($query_admin);
     ?>
     <title>Admin | <?= $admin['name']; ?></title>
+    <link rel="shortcut icon" href="assets/img/icon.png">
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
